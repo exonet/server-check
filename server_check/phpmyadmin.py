1e13f8f9a237e8ccdb73a30987266d102e2a5bb7
RvdH <r.vandenhof@exonet.nl>
2015-10-06 13:02:43 +0200

    Change the check string to support older phpMyAdmin versions

--- server_check/phpmyadmin.py
+++ server_check/phpmyadmin.py
@@ -18,7 +18,7 @@ def test_phpmyadmin():
                 passwd = value
 
     r = requests.get('http://localhost/phpmyadmin/', auth=(user, passwd))
-    if r.status_code == 200 and "User: %s@localhost" % user in r.text:
+    if r.status_code == 200 and "phpMyAdmin" in r.text:
         return "Logged in and authenticated"
     else:
         raise TestException("Unable to log in to phpMyAdmin with the '%s' user." % user)
