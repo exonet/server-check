51451a9a237d94150981ef99b48602e69a0b4e17
Henk Kraal <h.kraal@exonet.nl>
2019-07-11 10:55:13 +0200

    Update test_03_imap.py

--- tests/test_03_imap.py
+++ tests/test_03_imap.py
@@ -6,15 +6,15 @@ import pytest
 
 def test_00_test_imap(domain):
     with patch('imaplib.IMAP4') as mock_imap:
-        mock_imap.return_value.search.return_value = ['+OK', ['1 2 3']]
-        mock_imap.return_value.fetch.return_value = ['+OK:', [['', 'da_server_check mail test']]]
+        mock_imap.return_value.search.return_value = [b'+OK', [b'1 2 3']]
+        mock_imap.return_value.fetch.return_value = [b'+OK:', [['', b'da_server_check mail test']]]
 
         assert "Test message retrieved via Dovecot IMAP." in \
             imap.test_imap(domain.user, domain.domain, domain.password)
 
     with patch('imaplib.IMAP4_SSL') as mock_imap_ssl:
-        mock_imap_ssl.return_value.search.return_value = ['+OK', ['1 2 3']]
-        mock_imap_ssl.return_value.fetch.return_value = ['+OK:', [['', 'da_server_check mail test']]]
+        mock_imap_ssl.return_value.search.return_value = [b'+OK', [b'1 2 3']]
+        mock_imap_ssl.return_value.fetch.return_value = [b'+OK:', [[b'', b'da_server_check mail test']]]
 
         assert "Test message retrieved via Dovecot IMAP_SSL." in \
             imap.test_imap(domain.user, domain.domain, domain.password, True)
@@ -22,9 +22,9 @@ def test_00_test_imap(domain):
 
 def test_01_test_imap(domain):
     with patch('imaplib.IMAP4') as mock_imap:
-        mock_imap.return_value.search.return_value = ['+OK', ['1 2 3']]
-        mock_imap.return_value.fetch.return_value = ['+OK:', [['', 'unittest']]]
+        mock_imap.return_value.search.return_value = [b'+OK', [b'1 2 3']]
+        mock_imap.return_value.fetch.return_value = [b'+OK:', [[b'', b'unittest']]]
 
         with pytest.raises(exceptions.TestException) as err:
             imap.test_imap(domain.user, domain.domain, domain.password)
-        assert 'Retrieved message does not contain test string' in err.value.message
+            assert 'Retrieved message does not contain test string' in err.value.message
